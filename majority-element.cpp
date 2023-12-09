4090ba88293e93871fdc887b726d1dc4ec51f7b9
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-12-09 14:54:14 +0530

    majority element

---
+++ majority-element.cpp
@@ -0,0 +1,18 @@
+// LINK - https://www.interviewbit.com/problems/majority-element/
+// MEDIUM (without using extra space)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(const vector<int> &A) {
+    int ele=-1, cnt=0;
+    for(auto c : A){
+        if(cnt==0){ 
+            ele= c;
+            cnt++;
+        }
+        else if(c==ele) cnt++;
+        else cnt--;
+    }
+    return ele;
+}
